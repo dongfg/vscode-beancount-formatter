19da4940f5618b4613b50ff1d4c1d4e0c86b7819
dongfg <mail@dongfg.com>
2019-01-30 17:24:41 +0800

    call bean-format to format document

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as child from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,20 +9,31 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "vscode-beancount-formatter" is now active!');
+	console.log('[vscode-beancount-formatter] activated!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let disposable = vscode.languages.registerDocumentFormattingEditProvider({ scheme: 'file', language: 'beancount' }, {
+		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+			let formatterPath = vscode.workspace.getConfiguration("beancountFormatter")["binPath"];
+			let result = child.spawnSync(formatterPath, [], {
+				input: document.getText(),
+				cwd: process.cwd(),
+				env: process.env,
+				stdio: 'pipe',
+				encoding: 'utf-8'
+			});
+			if (result.output[1]) {
+				let rangeStart: vscode.Position = document.lineAt(0).range.start;
+				let rangeEnd: vscode.Position = document.lineAt(document.lineCount - 1).range.end;
+				return [vscode.TextEdit.replace(new vscode.Range(rangeStart, rangeEnd), result.output[1])];
+			} else if (result.output[2]) {
+				vscode.window.showInformationMessage(result.output[2]);
+			}
+			return [];
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
