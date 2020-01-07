3589d922003c9615e7c01d8ec35b55716465d283
dongfg <mail@dongfg.com>
2020-01-07 20:55:24 +0800

    build: config eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    '@typescript-eslint/class-name-casing': 'warn',
+    '@typescript-eslint/member-delimiter-style': [
+      'warn',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
+    '@typescript-eslint/semi': ['warn', 'always'],
+    curly: 'warn',
+    eqeqeq: ['warn', 'always'],
+    'no-redeclare': 'warn',
+    'no-throw-literal': 'warn',
+    'no-unused-expressions': 'warn',
+  },
+};
