8caf9d42675d6bb552f296a13f7f35a3be9a823e
dongfg <mail@dongfg.com>
2021-07-05 14:51:58 +0800

    bugfix: fix unit test

--- src/__tests__/format.test.ts
+++ src/__tests__/format.test.ts
@@ -1,5 +1,5 @@
 import { format, FormatOptions } from '../format';
-import { formatWithPython } from './helper';
+import { formatWithPython, dedent } from './helper';
 
 test('env', () => {
   expect(format('')).toBe(formatWithPython(''));
@@ -7,49 +7,28 @@ test('env', () => {
   // expect(format(' ')).toBe(formatWithPython(' '));
 });
 
-test('align_starts', () => {
-  const input = `
-* Section header
-
-;; Accounts (comments)
-2013-01-01 open Expenses:Restaurant
-2013-01-01 open Assets:Cash
-
-2014-03-02 * "Something"
-Expenses:Restaurant   50.02 USD
-  Assets:Cash
-
-2014-03-05 balance   Assets:Cash  -50.02 USD
-
-2014-03-10 * "Something"
-  Assets:Other   10 HOOL {500.23} USD ; Bla
-  Assets:Cash  
-`;
-  expect(format(input)).toBe(formatWithPython(input));
-});
-
 // testCase from https://github.com/beancount/beancount/blob/master/beancount/scripts/format_test.py
 
 test('success', () => {
-  const input = `
+  const input = dedent(`
 
-          * Section header
+  * Section header
 
-          ;; Accounts (comments)
-          2013-01-01 open Expenses:Restaurant
-          2013-01-01 open Assets:Cash
+  ;; Accounts (comments)
+  2013-01-01 open Expenses:Restaurant
+  2013-01-01 open Assets:Cash
 
-          2014-03-02 * "Something"
-            Expenses:Restaurant   50.02 USD
-            Assets:Cash
+  2014-03-02 * "Something"
+    Expenses:Restaurant   50.02 USD
+    Assets:Cash
 
-          2014-03-05 balance   Assets:Cash  -50.02 USD
+  2014-03-05 balance   Assets:Cash  -50.02 USD
 
-          2014-03-10 * "Something"
-            Assets:Other   10 HOOL {500.23} USD ; Bla
-            Assets:Cash  
+  2014-03-10 * "Something"
+    Assets:Other   10 HOOL {500.23} USD ; Bla
+    Assets:Cash
 
-`;
+`);
   const actual = format(input);
   // console.log(`actual: '${actual}'`);
   expect(actual).toBe(formatWithPython(input));
@@ -80,25 +59,25 @@ test('open_only_issue80', () => {
 });
 
 test('commas', () => {
-  const input = `
+  const input = dedent(`
 
-          * Section header
+  * Section header
 
-          ;; Accounts (comments)
-          2013-01-01 open Expenses:Restaurant
-          2013-01-01 open Assets:Cash
+  ;; Accounts (comments)
+  2013-01-01 open Expenses:Restaurant
+  2013-01-01 open Assets:Cash
 
-          2014-03-02 * "Something"
-            Expenses:Restaurant   1,050.02 USD
-            Assets:Cash
+  2014-03-02 * "Something"
+    Expenses:Restaurant   1,050.02 USD
+    Assets:Cash
 
-          2014-03-05 balance   Assets:Cash  -1,050.02 USD
+  2014-03-05 balance   Assets:Cash  -1,050.02 USD
 
-          2014-03-10 * "Something"
-            Assets:Other   10 HOOL {5,000.23} USD ; Bla
-            Assets:Cash
+  2014-03-10 * "Something"
+    Assets:Other   10 HOOL {5,000.23} USD ; Bla
+    Assets:Cash
 
-`;
+`);
   expect(format(input)).toBe(formatWithPython(input));
 });
 
@@ -109,7 +88,7 @@ test('currency_issue146', () => {
 
           2014-03-31 * "opening"
             Assets:Investments                 1.23 FOO_BAR
-            Equity:Opening-balances  
+            Equity:Opening-balances
 `;
   expect(format(input)).toBe(formatWithPython(input));
 });
@@ -121,24 +100,24 @@ test('fixed_width', () => {
 
         2016-08-02 * "" ""
           Expenses:Test     10.00 USD
-          Assets:Test  
+          Assets:Test
 `;
   const opts: FormatOptions = { prefixWidth: 40 };
   expect(format(input, opts)).toBe(formatWithPython(input, opts));
 });
 
 test('fixed_column', () => {
-  const input = `
-        2016-08-01 open Expenses:Test
-        2016-08-01 open Assets:Test
-        2016-08-01 balance Assets:Test  0.00 USD
+  const input = dedent(`
+  2016-08-01 open Expenses:Test
+  2016-08-01 open Assets:Test
+  2016-08-01 balance Assets:Test  0.00 USD
 
-        2016-08-02 * "" ""
-          Expenses:Test     10.00 USD
-          Assets:Test
-`;
+  2016-08-02 * "" ""
+    Expenses:Test     10.00 USD
+    Assets:Test
+`);
   const opts: FormatOptions = { currencyColumn: 50 };
-  expect(format(input, opts)).toBe(formatWithPython(input, opts));
+  expect(format(input, opts)).toBe(formatWithPython(input, opts).trimRight());
 });
 
 test('issue#8 1', () => {
@@ -153,7 +132,7 @@ test('issue#8 2', () => {
   const input = `
   2020-01-03 * "American Airlines" "ESHOPPING 2019 HOLIDAY BONUS"
   Income:PnC:Portal:AA
-  Assets:Points:AmericanAirlines  
+  Assets:Points:AmericanAirlines
 `;
   const opts: FormatOptions = { currencyColumn: 10 };
   expect(format(input, opts)).toBe(formatWithPython(input, opts));
